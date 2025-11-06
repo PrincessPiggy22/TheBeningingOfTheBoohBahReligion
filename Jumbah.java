4def2dfa4a3e8ee90404a33925ffa21f9353692a
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2025-11-06 09:26:47 -0500

    Update Jumbah.java

--- Jumbah.java
+++ Jumbah.java
@@ -9,6 +9,7 @@ public class Jumbah extends SupremeBoohbah{
 
     @Override
     public void command(){
+        super.command("...", true);
         System.out.println(this.name + " refuses to buy the overpriced Top Turner");
     }
 
