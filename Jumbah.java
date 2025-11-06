07b9eac9cb954052b90f27faa526a8a49fda69ec
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2025-11-06 09:15:03 -0500

    Create Jumbah.java

---
+++ Jumbah.java
@@ -0,0 +1,16 @@
+public class Jumbah extends SupremeBoohbah{
+
+    private String name;
+    Jumbah(){
+        super();
+        this.name = "Jumbah";
+        System.out.println(this.name + " does everything they can to prevent the Boohbah Supreme's power of capitalism from corrupting Boohbahland");
+    }
+
+    @Override
+    public void command(){
+        super.command("...");
+        System.out.println(this.name + " refuses to buy the overpriced Top Turner");
+    }
+
+}
