c847e45cb59722a82a23529f96b66a37751aed46
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2025-11-06 09:22:48 -0500

    Update SupremeBoohbah.java

--- SupremeBoohbah.java
+++ SupremeBoohbah.java
@@ -10,7 +10,7 @@ public class SupremeBoohbah {
         System.out.println(this.name + ", the Boohbah Supreme, commands all to buy the Playskool BoohBah Spinning Tops Set or PERISH PAINFULLY");
     }
 
-    public void command(String message){
+    public void command(String message, boolean e){ // w/ out the boolean the overload in Jingbah wouldn't work bc in the Arraylist, Jingbah was a SupremeBoohbah and wouldn't use any Jingbah stuff
         System.out.println(message);
     }
 
